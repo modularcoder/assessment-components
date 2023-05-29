26b10f9b54611ee9c9310c2ae66e9bd4558a64f9
Gevorg Harutyunyan <gevharut@gmail.com>
2023-05-29 18:50:24 +0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import TheHeader from './_components/TheHeader/TheHeader'
+
+function App() {
+  return (
+    <main>
+      <TheHeader />
+      <div className='container mx-auto px-4 py-14'>
+      <article className="prose lg:prose-xl">
+        <h1>Demo system design components</h1>
+
+        <h2>Autocomplete</h2>
+
+        <p>
+
+        </p>
+
+        <h2>Modal</h2>
+
+      </article>
+      </div>
+    </main>
+  )
+}
+
+
+export default App
